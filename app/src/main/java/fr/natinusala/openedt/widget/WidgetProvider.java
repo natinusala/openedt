11ec6401b5b5204bee378158d8c6bb3c420f4a7d
natinusala <natinusala@gmail.com>
2016-03-07 16:18:18 +0100

    Ajout des erreurs (rudimentaires) dans le Widget

--- app/src/main/java/fr/natinusala/openedt/widget/WidgetProvider.java
+++ app/src/main/java/fr/natinusala/openedt/widget/WidgetProvider.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ import fr.natinusala.openedt.utils.TimeUtils;
 public class WidgetProvider extends AppWidgetProvider
 {
     //TODO Configuration du groupe et de l'intervalle
-    //TODO Gérer les erreurs de chargement
+    //TODO Actualiser au toucher
 
     @Override
     public void onUpdate (Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
@@ -36,9 +37,8 @@ public class WidgetProvider extends AppWidgetProvider
         }
     }
 
-    class Task extends AsyncTask<Void, Void, Void>
+    class Task extends AsyncTask<Void, Void, ArrayList<Week>>
     {
-        ArrayList<Week> weeks;
         Context context;
         AppWidgetManager appWidgetManager;
         int wId;
@@ -53,31 +53,37 @@ public class WidgetProvider extends AppWidgetProvider
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            weeks = DataManager.getWeeksForGroup(context, group);
-            return null;
+        protected ArrayList<Week> doInBackground(Void... params) {
+            return DataManager.getWeeksForGroup(context, group);
         }
 
         @Override
-        protected void onPostExecute(Void param)
+        protected void onPostExecute(ArrayList<Week> weeks)
         {
-            Week week = WeekManager.getCurrentWeek(weeks);
-            Event event = WeekManager.getEventPerDay(week).get(0).get(0);
-
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
-            if (event != null)
+            if (weeks != null)
             {
-                Date dayDate = TimeUtils.createDateForDay(event.day, week);
-                SimpleDateFormat sdf = TimeUtils.createDateFormat();
+                Week week = WeekManager.getCurrentWeek(weeks);
+                Event event = WeekManager.getEventPerDay(week).get(0).get(0);
 
-                views.setTextViewText(R.id.widget_date, String.format("Le %s (semaine %d)", sdf.format(dayDate), week.num));
-                views.setInt(R.id.widget_title, "setBackgroundColor", Color.parseColor(event.colour));
-                views.setTextViewText(R.id.widget_rooms, String.format("En salle %s", event.getPrettyRoom()));
-                views.setTextViewText(R.id.widget_hour, String.format("De %s à %s", event.starttime, event.endtime));
-                views.setTextViewText(R.id.widget_module, event.createCategoryModule());
-                views.setTextViewText(R.id.widget_staff, String.format("Avec %s", event.getPrettyStaff()));
-                views.setTextViewText(R.id.widget_group, group.name + " - prochain cours :");
+                if (event != null)
+                {
+                    Date dayDate = TimeUtils.createDateForDay(event.day, week);
+                    SimpleDateFormat sdf = TimeUtils.createDateFormat();
+
+                    views.setTextViewText(R.id.widget_date, String.format("Le %s (semaine %d)", sdf.format(dayDate), week.num));
+                    views.setInt(R.id.widget_title, "setBackgroundColor", Color.parseColor(event.colour));
+                    views.setTextViewText(R.id.widget_rooms, String.format("En salle %s", event.getPrettyRoom()));
+                    views.setTextViewText(R.id.widget_hour, String.format("De %s à %s", event.starttime, event.endtime));
+                    views.setTextViewText(R.id.widget_module, event.createCategoryModule());
+                    views.setTextViewText(R.id.widget_staff, String.format("Avec %s", event.getPrettyStaff()));
+                    views.setTextViewText(R.id.widget_group, group.name + " - prochain cours :");
+                }
+            }
+            else
+            {
+                Toast.makeText(context, "Impossible d'actualiser le widget d'OpenEDT", Toast.LENGTH_LONG).show();
             }
 
             appWidgetManager.updateAppWidget(wId, views);
