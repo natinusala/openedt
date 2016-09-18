5a500249dd27cf60d3a5dc0a1c81d10867f0d969
maveist <maveist44@gmail.com>
2016-09-18 22:15:25 +0200

    select time and validate done.

--- app/src/main/java/fr/natinusala/openedt/activity/ParametersActivity.java
+++ app/src/main/java/fr/natinusala/openedt/activity/ParametersActivity.java
@@ -1,6 +1,7 @@
 package fr.natinusala.openedt.activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,33 +10,80 @@ import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import fr.natinusala.openedt.R;
 
+
+
 public class ParametersActivity extends Activity {
+    public static final String SMART_ALARM_PREF = "SMART_ALARM";
+    //public static final String SMART_ALARM_TIME = "SMART_ALARM_TIME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parameters);
         Switch smartAlarm = (Switch) this.findViewById(R.id.switch_smart_alarm);
+        SharedPreferences preferences = getSharedPreferences(SMART_ALARM_PREF, 0);
+        boolean isEnabled = preferences.getBoolean("isEnabled", false);
+        smartAlarm.setChecked(isEnabled);
+        checkSmartAlarmEnabled(smartAlarm);
         smartAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkSmartAlarmEnabled((Switch) v);
+                Switch smartAlarm = (Switch) v;
+                checkSmartAlarmEnabled(smartAlarm);
+                SharedPreferences preferences = getSharedPreferences(SMART_ALARM_PREF, 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("isEnabled", smartAlarm.isChecked());
+                editor.commit();
+            }
+        });
+
+
+        //select time
+
+        Button validateButton = (Button) findViewById(R.id.validate_time);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner hours = (Spinner) findViewById(R.id.hours_spinner);
+                Spinner minutes = (Spinner) findViewById(R.id.minutes_spinner);
+                setTime(hours, minutes);
             }
         });
     }
 
+    public void setTime(Spinner hours, Spinner minutes){
+        SharedPreferences preferences = getSharedPreferences(SMART_ALARM_PREF, 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        int nb_hours = Integer.parseInt(hours.getSelectedItem().toString());
+        int nb_minutes = Integer.parseInt(minutes.getSelectedItem().toString());
+        int pref_time= nb_minutes + nb_hours * 60;
+        editor.putInt("time", pref_time);
+        editor.commit();
+        String str = "Alarme réglée à";
+        if(nb_hours > 0){
+            str = str + " " + Integer.toString(nb_hours)+ "heure(s)";
+        }
+        if(nb_minutes > 0){
+            str = str + " " + Integer.toString(nb_minutes) + "minute(s)";
+        }
+        Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
+    }
+
     public void checkSmartAlarmEnabled(Switch smartAlarm){
-        if(smartAlarm.isActivated()){
-            LinearLayout layout = (LinearLayout) this.findViewById(R.id.time_picker);
+        LinearLayout layout = (LinearLayout) this.findViewById(R.id.time_picker);
+        if(smartAlarm.isChecked()){
+
             ArrayList<String> hours = new ArrayList<String>();
             for (int i = 0; i <= 24; i++){
                 hours.add(Integer.toString(i));
@@ -61,6 +109,9 @@ public class ParametersActivity extends Activity {
                     (android.R.layout.simple_spinner_dropdown_item);
 
             spinnerMinutes.setAdapter(minutesAdapter);
+            layout.setVisibility(View.VISIBLE);
+        }else{
+            layout.setVisibility(View.GONE);
         }
     }
 
